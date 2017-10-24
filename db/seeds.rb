d1fecd13cd43f225d626bfc8c2ccba85e5f7208e
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-24 10:59:25 -0700

    Seeds the database and adds a simple index.html.erb view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.destroy_all
+d1 = DateTime.new(2013, 6, 22)
+cat1 = Cat.create!(name: 'Teddy', birth_date: d1, color: 'black', sex: 'M', description: 'A fluffy little cat')
+cat2 = Cat.create!(name: 'Cyndi', birth_date: d1, color: 'brown', sex: 'F', description: 'A small cute cat')
+cat3 = Cat.create!(name: 'Pishi', birth_date: d1, color: 'white', sex: 'M', description: 'A grumpy cat')
