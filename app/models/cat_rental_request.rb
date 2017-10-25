616088807d0733a06ceed8bf2648a8fd9c6020b4
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-24 17:11:37 -0700

    Adds costum validations for overlapping dates

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,8 +1,22 @@
+# == Schema Information
+#
+# Table name: cat_rental_requests
+#
+#  id         :integer          not null, primary key
+#  cat_id     :integer          not null
+#  start_date :date             not null
+#  end_date   :date             not null
+#  status     :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class CatRentalRequest < ApplicationRecord
   STATS = %w(PENDING APPROVED DENIED).freeze
 
   validates :status, inclusion: STATS
   validates :start_date, :end_date, :status, presence: true
+  validates :does_not_overlap_approved_request
 
   after_initialize :assign_pending_status
 
@@ -16,5 +30,26 @@ class CatRentalRequest < ApplicationRecord
     def assign_pending_status
       self.status ||= 'PENDING'
     end
+
+    def overlapping_requests
+      CatRentalRequest
+        .where.not(id: self.id)
+        .where(cat_id: cat_id)
+        .where.not('start_date > :end_date OR end_date < :start_date',
+          start_date: start_date, end_date: end_date)
+    end
+
+    def overlapping_approved_requests
+      overlapping_requests.where('status = \'APPROVED\'')
+    end
+
+    def does_not_overlap_approved_request
+    return if self.denied?
+
+    unless overlapping_approved_requests.empty?
+      errors[:base] <<
+        'Cannot approve request'
+    end
+  end
 
 end
--- db/migrate/20171024225956_create_cat_rental_requests_table.rb
+++ db/migrate/20171024225956_create_cat_rental_requests_table.rb
@@ -1,10 +1,10 @@
 class CreateCatRentalRequestsTable < ActiveRecord::Migration[5.1]
   def change
     create_table :cat_rental_requests do |t|
-      t.integer cat_id, null: false
-      t.date start_date, null: false
-      t.date end_date, null: false
-      t.string status, null: false
+      t.integer :cat_id, null: false
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+      t.string :status, null: false
 
       t.timestamps
     end
